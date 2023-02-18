88c38089062aa3bc99931ecd84bc71cdac8ab7c6
ybkuroki <45133652+ybkuroki@users.noreply.github.com>
2023-02-18 16:31:12 +0900

    Fix the unit test for props

--- util/props_test.go
+++ util/props_test.go
@@ -10,13 +10,23 @@ import (
 //go:embed test.properties
 var testPropsFile embed.FS
 
-func TestReadPropertiesFile(t *testing.T) {
+func TestReadPropertiesFile_FileExists(t *testing.T) {
 	messages := ReadPropertiesFile(testPropsFile, "test.properties")
 
 	assert.Equal(t, "testtest", messages["props1"])
 	assert.Equal(t, "test test", messages["props2"])
 	assert.Equal(t, "", messages["props3"])
+
 	_, ok := messages["props4"]
 	assert.Equal(t, false, ok)
+
 	assert.Equal(t, "test #test", messages["props5"])
+
+	_, ok = messages["# comment line"]
+	assert.Equal(t, false, ok)
+}
+
+func TestReadPropertiesFile_FileIsNil(t *testing.T) {
+	messages := ReadPropertiesFile(testPropsFile, "testnil.properties")
+	assert.Equal(t, map[string]string(nil), messages)
 }
