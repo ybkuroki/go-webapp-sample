e75c657894be6fc216c0cb7a47e616e12d8876ba
pawan-tomar <103077944+pawan-tomar@users.noreply.github.com>
2024-06-05 00:00:01 +0200

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,26 @@
+# outputs.tf
+
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.docker_server.public_ip
+}
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.docker_server.id
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.docker_vpc.id
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.docker_subnet.id
+}
+
+output "security_group_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.docker_server_sg.id
+}
