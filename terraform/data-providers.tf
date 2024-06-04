a9f1de53f0f69be77c64dcf7b556a5e27b66f106
pawan-tomar <103077944+pawan-tomar@users.noreply.github.com>
2024-06-04 23:59:43 +0200

    Create data-providers.tf

---
+++ terraform/data-providers.tf
@@ -0,0 +1,10 @@
+# data-providers.tf
+
+data "aws_ami" "aws_linux_2_latest" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+}
