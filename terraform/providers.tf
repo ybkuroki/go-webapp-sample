f6c11448ab1fba292a10a07c68f66fdc551550da
pawan-tomar <103077944+pawan-tomar@users.noreply.github.com>
2024-06-05 19:13:59 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,15 +1,1 @@
-# providers.tf
 
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.74.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
