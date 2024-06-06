334c79c3903ecd4c163caabab6ae475c76ef58fc
pawan-tomar <103077944+pawan-tomar@users.noreply.github.com>
2024-06-06 15:22:40 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "aws_security_group_http_server_details" {
 output "http_server_public_dns" {
   value = aws_instance.http_server.public_dns
 }
+
+output "instance_key_name" {
+  value = aws_instance.http_server.key_name
+}
+
+output "instance_public_ip" {
+  value = aws_instance.http_server.public_ip
+}
