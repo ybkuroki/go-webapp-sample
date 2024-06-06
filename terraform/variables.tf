6ecb8eb0489b71b5c2ddefd426ce1357af94f4c4
pawan-tomar <103077944+pawan-tomar@users.noreply.github.com>
2024-06-06 15:22:24 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "ssh_private_key_path" {
+  description = "Path to the SSH private key"
+}
 
+variable "DOCKER_USERNAME" {
+  description = "Username for Docker Hub login"
+}
+
+variable "DOCKER_PASSWORD" {
+  description = "Password for Docker Hub login"
+}
