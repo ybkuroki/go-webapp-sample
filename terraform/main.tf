74af59e118c1037ec69f65b4cd9c1c1efc19346a
pawan-tomar <103077944+pawan-tomar@users.noreply.github.com>
2024-06-08 00:02:44 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,46 +1,80 @@
-provider "aws" {
-  region = "us-east-1"
+# main.tf
+
+resource "aws_internet_gateway" "docker_igw" {
+  vpc_id = aws_vpc.docker_vpc.id
 }
 
-resource "aws_default_vpc" "default" {}
+resource "aws_route_table" "docker_route_table" {
+  vpc_id = aws_vpc.docker_vpc.id
 
-resource "aws_security_group" "http_server_sg" {
-  name = "http_server_sg"
-  vpc_id = aws_default_vpc.default.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.docker_igw.id
+  }
+}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_route_table_association" "docker_route_table_association" {
+  subnet_id      = aws_subnet.docker_subnet.id
+  route_table_id = aws_route_table.docker_route_table.id
+}
+
+resource "aws_vpc" "docker_vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "docker_subnet" {
+  vpc_id            = aws_vpc.docker_vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = "${var.region}a"
+  map_public_ip_on_launch = true  # Add this line
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+
+
+resource "aws_security_group" "docker_server_sg" {
+  name   = var.security_group_name
+  vpc_id = aws_vpc.docker_vpc.id
+
+  dynamic "ingress" {
+    for_each = var.allowed_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = var.cidr_blocks
+    }
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "egress" {
+    for_each = var.egress_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = -1
+      cidr_blocks = var.cidr_blocks
+    }
   }
 
   tags = {
-    name = "http_server_sg"
+    Name = var.security_group_name
   }
 }
 
-resource "aws_instance" "http_server" {
+resource "aws_instance" "docker_server" {
   ami                    = data.aws_ami.aws_linux_2_latest.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.docker_server_sg.id]
+  subnet_id              = aws_subnet.docker_subnet.id
+  associate_public_ip_address = true
   key_name               = "default-ec2"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
-  subnet_id              = data.aws_subnets.default_subnets.ids[0]
 
+  
   provisioner "remote-exec" {
     inline = [
       "sudo yum update -y",
@@ -58,4 +92,6 @@ resource "aws_instance" "http_server" {
     user        = "ec2-user"
     private_key = file(var.ssh_private_key_path)
   }
+
+  tags = var.tags
 }
