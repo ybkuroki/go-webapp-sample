5c1668846687b3351819e36c67395efa57237c77
pawan-tomar <103077944+pawan-tomar@users.noreply.github.com>
2024-06-08 00:03:13 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,41 @@
-output "aws_security_group_http_server_details" {
-  value = aws_security_group.http_server_sg
-}
-
-output "http_server_public_dns" {
-  value = aws_instance.http_server.public_dns
-}
-
-output "instance_key_name" {
-  value = aws_instance.http_server.key_name
-}
+# outputs.tf
 
 output "instance_public_ip" {
-  value = aws_instance.http_server.public_ip
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.docker_server.public_ip
+}
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.docker_server.id
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.docker_vpc.id
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.docker_subnet.id
+}
+
+output "security_group_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.docker_server_sg.id
+}
+
+output "allowed_ports" {
+  description = "List of ingress ports allowed"
+  value       = var.allowed_ports
+}
+
+output "egress_ports" {
+  description = "List of egress ports allowed"
+  value       = var.egress_ports
+}
+
+output "cidr_blocks" {
+  description = "CIDR blocks allowed for access"
+  value       = var.cidr_blocks
 }
