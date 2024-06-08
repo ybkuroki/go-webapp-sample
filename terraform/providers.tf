8292aafe3b90f6ebee2dc6133a7ac400ba523a0e
pawan-tomar <103077944+pawan-tomar@users.noreply.github.com>
2024-06-08 12:35:11 +0200

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,6 +8,15 @@ terraform {
       version = "3.74.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-remote-backend-test04"
+    key    = "global/mystatefile/terraform.tfstate"
+    region = "us-east-1"
+    # Optional: Uncomment this line if you want to enable DynamoDB table for state locking
+    dynamodb_table = "statelockfile"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
